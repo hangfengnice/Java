e5546a1cf747ca327d69230b26a9023ded6aef80
hangfengnice <nicehangfeng@foxmail.com>
2019-08-16 23:06:22 +0800

    hello java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    // long num1 = 200000000;
+    // byte num4 = 40, num5 = 50;
+    // int result = num4 + num5;
+    // boolean num6 = false;
+    // // int result1 = num1 + num6;
+    // System.out.println(num1);
+    // System.out.println(result);
+    char zifu1 = '1';
+    char zifu2 = '中';
+    System.out.println(zifu1 + 0);
+    System.out.println(true && false);
+  }
+
+}
